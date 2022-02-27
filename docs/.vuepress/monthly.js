c517ccc156fd7a916c647fd872d49e5d67cec75f
kim-jin-seop <33629459+kim-jin-seop@users.noreply.github.com>
2022-02-27 14:59:32 +0900

    vuepress

--- docs/.vuepress/monthly.js
+++ docs/.vuepress/monthly.js
@@ -1,36 +1,3 @@
-// .vuepress/config.js
-var path = require('path')
-let {getArticles} = require(path.resolve('.vuepress/monthly'))
-
-module.exports = {
-  title: "TIL : Today I Learned",
-  description: "매일 공부한 내용을 정리합니다.",
-  themeConfig: {
-    sidebar: [
-           {
-               title: 'Vuepress',
-               collapsable: true,
-               children: getArticles('vuejs/vuepress')
-           }
-       ],
-    nav: [{ text: "Github", link: "https://github.com/kim-jin-seop" },
-          { text: "Blog", link: "https://cnu-jinseop.tistory.com/"}],
-    sidebar: getSidebarArr()
-  },
-  markdown: {
-    lineNumbers: true
-  },
-  //가장 중요한 부분!
-  //<username>.github.io 뒤에 주소가 붙으시면
-  //아래와 같이 뒤 붙는 주소를 넣어주셔야합니다.
-  //안그러면 css 가 반영이 안되요!! 꼭꼭 넣어주세요
-  base: "/TIL/",
-  plugins:[
-    ["vuepress-plugin-code-copy"],
-    ["@vuepress/back-to-top"]
-  ]
-};
-
 function getSidebarArr() {
   var fs = require("fs");
   var docsPath = __dirname + "/../";
@@ -80,12 +47,4 @@ function makeSidebarObject(folder, mdfileList) {
     title: title,
     children: mdfileList
   };
-}
-function aheadOfReadme(arr) {
-  // ['1.test.md','README.md'] => ['README.md','1.test.md']
-  var readmeIdx = arr.indexOf("README.md");
-  if (readmeIdx > 0) {
-    arr.unshift(arr.splice(readmeIdx, 1)[0]);
-  }
-  return arr;
 }
